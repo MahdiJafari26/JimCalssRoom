47519ad853ac61bf9aefbea951538c88be3872d9
MahdiJafari26 <mahdijafari1379@gmail.com>
2023-12-28 14:23:40 +0330

    Initial commit

---
+++ JimCalssRoom/src/main/java/Jafari/Mahdi/JimCalssRoom/JimCalssRoomApplication.java
@@ -0,0 +1,13 @@
+package Jafari.Mahdi.JimCalssRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimCalssRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimCalssRoomApplication.class, args);
+	}
+
+}
---
+++ JimCalssRoom/src/test/java/Jafari/Mahdi/JimCalssRoom/JimCalssRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package Jafari.Mahdi.JimCalssRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimCalssRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
